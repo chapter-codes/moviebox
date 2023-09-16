fb3704903181230ff081a008402d2d5dec4c9d3e
Churchill Nnanna <68286428+chapter-codes@users.noreply.github.com>
2023-09-16 23:30:36 +0100

    Update Header.jsx

--- src/components/standalone/Header.jsx
+++ src/components/standalone/Header.jsx
@@ -1,5 +1,5 @@
 import Logo from '../../assets/tv.svg'
-import SearchIcon from '../../assets/search.svg'
+import SearchIcon from '../../assets/Search.svg'
 import MenuIcon from '../../assets/Menu.svg'
 
 
